c2233ff3d90729485305c5c0d166ec075c75c38a
Jenny Lin <misslinyang91@gmail.com>
2021-09-08 18:35:07 +1000

    set the dimensions

--- app.js
+++ app.js
@@ -15,3 +15,62 @@ const COLOR_AI = "lawngreen";
 const COLOR_AI_DARK = "darkgreen";
 const COLOR_RI = "orange";
 const COLOR_RI_DARK = " darkgoldenrod";
+
+//the game loop
+let timeDiff, timeLast;
+requestAnimationFrame(playGame);
+function playGame(timeNow) {
+  //initialize timeLast
+  if (!timeLast) {
+    timeLast = timeNow;
+  }
+
+  //calculate the time diff
+  timeDiff = (timeNow - timeLast) / 1000; //seconds
+  timeLast = timeNow;
+
+  //calling the next frame
+  requestAnimationFrame(playGame);
+}
+
+//newGame function
+function newGame() {
+  createGrid();
+}
+
+//createGrid function
+function createGrid() {
+  grid = [];
+
+  //set up cell size and margins
+  let cell, marginX, marginY;
+
+  //device portrait orientation
+  if (((width - margin * 2) * GRID_ROWS) / GRID_COLS < height - margin * 2) {
+    cell = (width * margin * 2) / GRID_COLS;
+    marginX = margin;
+    marginY = (height - cell * GRID_ROWS) / 2;
+  }
+  //device landscape orientation
+  else {
+    cell = (height - margin * 2) / GRID_ROWS;
+    marginY = margin;
+    marginX = width - (cell * GRID_COLS) / 2;
+  }
+}
+//draw background function
+function drawBackground() {
+  canvasContext.fillStyle = COLOR_BG;
+  canvasContext.fillRect(0, 0, width, height);
+}
+
+//setdimensions function
+function setDimensions() {
+  width = window.innerWidth;
+  height = window.innerHeight;
+  canvasEl.width = width;
+  canvasEl.height = height;
+  margin = MARGIN * Math.min(height, width);
+
+  newGame();
+}
